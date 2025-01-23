88b6bad5a9c0c4be63b6a884f12b7b33fe68315f
AlexBoiko412 <bojkosasa78@gmail.com>
2025-01-23 21:01:18 +0100

    Sanity Init

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,8 +1,6 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-import { dataset, projectId } from '../env'
-
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
