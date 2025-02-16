55765ba4996c45c1aa1b48f50854a5dda2a78f6c
AlexBoiko412 <bojkosasa78@gmail.com>
2025-02-16 09:14:35 +0100

    queries

---
+++ sanity/lib/queries.ts
@@ -0,0 +1,13 @@
+import {defineQuery} from "groq";
+
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)]  | order("_createdAt") {
+  _id,
+    image,
+    title,
+    author -> {_id, name, image, bio},
+    views,
+    pitch,
+    category,
+    slug,
+    description
+}`)
