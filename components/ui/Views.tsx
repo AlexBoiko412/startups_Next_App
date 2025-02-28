6424f2b4d834f6199a145a91fb4a2b88982f5885
Alex <bojkosasa78@gmail.com>
2025-02-28 19:45:33 +0100

    Views Updating

--- components/ui/Views.tsx
+++ components/ui/Views.tsx
@@ -2,12 +2,19 @@ import {Eye} from "lucide-react";
 import React from "react";
 import {client} from "@/sanity/lib/client";
 import {STARTUP_VIEWS} from "@/sanity/lib/queries";
+import {writeClient} from "@/sanity/lib/write-client";
+import {after} from "next/server";
 
 
 export default async function Views({id}: {id: string}) {
     const {views} = await client.withConfig({
         useCdn: false
     }).fetch(STARTUP_VIEWS, { id })
+
+    after(async () => {
+        await writeClient.patch(id).set({views: views + 1}).commit()
+    })
+
 
     return (
         <div className={"flex items-center gap-1 leading-1"}>
