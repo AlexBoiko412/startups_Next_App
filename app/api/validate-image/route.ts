cca8b1b39bb3d814cff010ec4084c6a4ded4d4df
Alex <bojkosasa78@gmail.com>
2025-03-06 18:25:37 +0100

    Misc

--- app/api/validate-image/route.ts
+++ app/api/validate-image/route.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest } from "next";
 import {NextResponse} from "next/server";
 
 export async function GET(req: NextApiRequest) {
