eb8b37b91c566985930e94e6513d8b16277c6859
Alex <bojkosasa78@gmail.com>
2025-03-07 15:27:45 +0100

    Save Form Fields Fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Navbar} from "@/components/Navbar";
-import {Toaster} from "@/components/ui/sonner";
 
 const Layout = ({children}: Readonly<{ children: React.ReactNode }>) => {
     return (
